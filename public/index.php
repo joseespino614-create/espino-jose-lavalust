1a5812af8f10c8e89a8eeb4d48401e2277334016
Jose Espino <joseespino614@gmail.com>
2026-08-10 16:24:44 +0800

    Test Render automatic deployment

--- public/index.php
+++ public/index.php
@@ -83,5 +83,6 @@ define('PUBLIC_DIR', $public_folder);
  * Setup done? Then Hurray!
  * ------------------------------------------------------
  */
+// Lab 2 - Render automatic deployment test
 require_once SYSTEM_DIR . 'kernel/LavaLust.php';
 ?>
