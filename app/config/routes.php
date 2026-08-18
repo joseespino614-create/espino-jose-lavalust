d079b8d24d1afbdd2bfcf92a5134aa2eeb556b7e
Jose Espino <joseespino614@gmail.com>
2026-08-18 20:59:03 +0800

    Update Student Information System

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,9 +44,9 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'StudentController::index');
 
 $router->get('/student', 'StudentController::index');
 
 $router->get('/student/profile', 'StudentController::profile')
-       ->middleware('StudentMiddleware');
+       ->middleware('student');
